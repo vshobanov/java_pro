00167b31d8b20f20fab71fcc8ab43ae19a25a3b2
Шобанов Владимир Александрович <vashobanov@dev.vtb.ru>
2024-02-01 10:46:29 +0300

    Homework lesson3

---
+++ ru/inno/lesson3/Employee.java
@@ -0,0 +1,41 @@
+package ru.inno.lesson3;
+
+public class Employee {
+   private String name;
+    private  int age;
+      String position;
+
+
+    public Employee(String name, int age, String position) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+}
--- ru/inno/lesson3/MainApplication.java
+++ ru/inno/lesson3/MainApplication.java
@@ -1,4 +1,75 @@
 package ru.inno.lesson3;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
 public class MainApplication {
+    public static void main(String[] args) throws IOException {
+
+        String inputString = "Имеется массив строк в каждой из которых лежит набор из 5 строк разделенных пробелом найдите среди всех слов самое длинное если таких слов несколько получите любое из них";
+
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            list.add(random.nextInt(100));
+        }
+        System.out.println("Исходный массив:" + list);
+        System.out.println("Дедублицированный массив:" + list.stream()
+                .distinct()
+                .toList());
+        System.out.println("3-е наибольшее число (без дедубликации):" + list.stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(2)
+                .findFirst()
+                .stream()
+                .toList());
+        System.out.println("3-е наибольшее число:" + list.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(2)
+                .findFirst()
+                .stream()
+                .toList());
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Alice", 35, "Engineer"));
+        employees.add(new Employee("Bob", 21, "Engineer"));
+        employees.add(new Employee("Sasha", 45, "Engineer"));
+        employees.add(new Employee("Vova", 50, "Manager"));
+        employees.add(new Employee("Alex", 50, "Chief"));
+        employees.add(new Employee("Ivan", 28, "Engineer"));
+
+        System.out.println("Список имен 3-х инженеров:" + employees.stream()
+                .filter(employee -> employee.getPosition().equals("Engineer"))
+                .sorted(Comparator.comparingInt(Employee::getAge).reversed())
+                .limit(3)
+                .map(Employee::getName)
+                .toList());
+        System.out.println("Средний возраст сотрудников:" + employees.stream()
+                .filter(employee -> employee.getPosition().equals("Engineer"))
+                .mapToInt(Employee::getAge)
+                .average()
+                .orElse(0));
+
+
+        Map<String, Integer> wordFrequency = Arrays.stream(inputString.split(" "))
+                .collect(Collectors.toMap(x -> x, x -> 1, Integer::sum));
+        System.out.println(wordFrequency);
+
+        System.out.println(Arrays.stream(inputString.split(" "))
+                .sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()))
+                .toList());
+
+        System.out.println("Самое длинное слово в файле:" + Files.lines(Paths.get("ru/inno/lesson3/words.txt"))
+                .map(line -> (line.split(" ")))
+                .flatMap(Arrays::stream)
+                .sorted(Comparator.comparing(String::length).reversed()).limit(1).toList());
+    }
+
 }
