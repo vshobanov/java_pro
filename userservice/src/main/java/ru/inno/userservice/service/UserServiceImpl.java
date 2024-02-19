68f0157007612218707f98f8b500aa65289c23c4
Шобанов Владимир Александрович <vashobanov@dev.vtb.ru>
2024-02-19 15:55:53 +0300

    userservice fixed injection

--- userservice/src/main/java/ru/inno/userservice/service/UserServiceImpl.java
+++ userservice/src/main/java/ru/inno/userservice/service/UserServiceImpl.java
@@ -9,33 +9,34 @@ import java.util.List;
 
 @Component
 public class UserServiceImpl implements UserService {
-    private  UserDaoImpl userDaoImpl;
-    @Autowired
-    public void setUserDaoImpl(UserDaoImpl userDaoImpl) {
-        this.userDaoImpl = userDaoImpl;
+    private  UserDao userDao;
+@Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
     }
+
     @Override
     public void createUser(Long id, String name) {
-        userDaoImpl.addUser(id, name);
+        userDao.addUser(id, name);
     }
     @Override
     public void deleteUser(Long id) {
-        userDaoImpl.deleteUser(id);
+        userDao.deleteUser(id);
     }
     @Override
     public List<User> selectUsers() {
-        return userDaoImpl.getAllUsers();
+        return userDao.getAllUsers();
     }
     @Override
     public void updateUser(Long id, String newName) {
-        userDaoImpl.updateUser(id, newName);
+        userDao.updateUser(id, newName);
     }
     @Override
     public Integer maxId() {
-        return userDaoImpl.maxIdUsers();
+        return userDao.maxIdUsers();
     }
     @Override
     public User getUserById(Long id) {
-        return userDaoImpl.getUserById(id);
+        return userDao.getUserById(id);
     }
 }
